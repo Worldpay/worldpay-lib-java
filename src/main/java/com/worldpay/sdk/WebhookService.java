ce962536cee16a6095605e3155ba418e3dc6d3a2
sudhakarvajravel <sudhakar.vajravel@gmail.com>
2014-10-31 16:21:07 +0000

    PB-64  Added new api and test cases

--- src/main/java/com/worldpay/sdk/WebhookService.java
+++ src/main/java/com/worldpay/sdk/WebhookService.java
@@ -1,12 +1,16 @@
 package com.worldpay.sdk;
 
 import com.worldpay.gateway.clearwater.client.ui.dto.common.AbstractNotificationPayload;
+import com.worldpay.gateway.clearwater.client.ui.dto.request.WebhookRequest;
+import com.worldpay.gateway.clearwater.client.ui.dto.response.WebhookListResponse;
+import com.worldpay.gateway.clearwater.client.ui.dto.response.WebhookResponse;
 
 /**
  * Service for handlig incoming webhook notifications
  */
 public class WebhookService extends AbstractService {
 
+    public static final String WEBHOOK_URL = "/merchants/%s/settings/webhooks";
     /**
      * Constructor
      *
@@ -25,5 +29,13 @@ public class WebhookService extends AbstractService {
      */
     public AbstractNotificationPayload process(String requestBody) {
         return http.handleRequest(requestBody, AbstractNotificationPayload.class);
+    }
+
+    public WebhookResponse create(String merchantId, WebhookRequest request) {
+        return http.post(String.format(WEBHOOK_URL, merchantId), request, WebhookResponse.class);
+    }
+
+    public WebhookListResponse getWebhooks(String merchantId) {
+        return http.get(String.format(WEBHOOK_URL, merchantId), WebhookListResponse.class);
     }
 }
--- src/test/java/com/worldpay/sdk/WebhookServiceTest.java
+++ src/test/java/com/worldpay/sdk/WebhookServiceTest.java
@@ -1,19 +1,23 @@
 package com.worldpay.sdk;
 
 import com.worldpay.gateway.clearwater.client.core.dto.common.Environment;
+import com.worldpay.gateway.clearwater.client.core.exception.WorldpayException;
 import com.worldpay.gateway.clearwater.client.ui.dto.common.AbstractNotificationPayload;
 import com.worldpay.gateway.clearwater.client.ui.dto.common.NotificationEventType;
 import com.worldpay.gateway.clearwater.client.ui.dto.order.OrderStatusChangeNotificationPayload;
+import com.worldpay.gateway.clearwater.client.ui.dto.request.WebhookRequest;
+import com.worldpay.gateway.clearwater.client.ui.dto.response.WebhookListResponse;
+import com.worldpay.gateway.clearwater.client.ui.dto.response.WebhookResponse;
 import com.worldpay.sdk.util.JsonParser;
 import com.worldpay.sdk.util.PropertyUtils;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.management.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
 
 /**
@@ -21,17 +25,25 @@ import static org.hamcrest.core.IsInstanceOf.instanceOf;
  */
 public class WebhookServiceTest {
 
-    private WebhookService webhookService;
+    public static final String WEB_HOOK_URL = "https://www.google.com";
 
     private final String merchantId = "Merchant";
+
     private final String aggregateMerchantId = "AggregateMerchant";
+
     private final String merchantCode = "MerchantCode";
+
     private final String adminCode = "adminCode";
+
     private final String orderCode = "orderCode";
+
     private final String paymentStatus = "paid";
+
     private final Environment environment = Environment.TEST;
+
     private final NotificationEventType notificationEventType = NotificationEventType.ORDER_STATE_CHANGE;
 
+    private WebhookService webhookService;
 
     @Before
     public void setup() {
@@ -45,7 +57,7 @@ public class WebhookServiceTest {
         AbstractNotificationPayload finalPayload = webhookService.process(requestBody);
         assertThat("Notification Type", finalPayload, instanceOf(OrderStatusChangeNotificationPayload.class));
 
-        OrderStatusChangeNotificationPayload orderNotification = (OrderStatusChangeNotificationPayload)finalPayload;
+        OrderStatusChangeNotificationPayload orderNotification = (OrderStatusChangeNotificationPayload) finalPayload;
 
         assertThat("Order notification", orderNotification, is(notNullValue()));
         assertThat("Payment status", orderNotification.getPaymentStatus(), is(paymentStatus));
@@ -57,19 +69,73 @@ public class WebhookServiceTest {
         assertThat("Notification event type", orderNotification.getNotificationEventType(), is(notificationEventType));
     }
 
+    @Test
+    public void shouldCreateWebhook() {
+        WebhookRequest request = new WebhookRequest();
+        List<String> events = new ArrayList<String>();
+        events.add("ALL");
+        request.setEvents(events);
+        request.setWebHookUrl(WEB_HOOK_URL);
+        WebhookResponse response = webhookService.create(PropertyUtils.getProperty("merchantId"), request);
+        assertThat("webhook url", response.getWebHookId(), is(notNullValue()));
+        assertThat("webhook events", response.getEvents().size(), is(greaterThan(0)));
+    }
+
+    @Test
+    public void shouldNotCreateWebhookWithInvalidEvent() {
+        WebhookRequest request = new WebhookRequest();
+        List<String> events = new ArrayList<String>();
+        events.add("invalid-event");
+        request.setEvents(events);
+        request.setWebHookUrl(WEB_HOOK_URL);
+        try {
+            webhookService.create(PropertyUtils.getProperty("merchantId"), request);
+        } catch (WorldpayException e) {
+            assertThat("Invalid event type", e.getApiError().getCustomCode(), is("BAD_REQUEST"));
+            assertThat("Invalid event type", e.getApiError().getMessage(), is("Invalid event type"));
+        }
+    }
+
+    @Test
+    public void shouldNotCreateWebhookWithInvalidWebhookUrl() {
+        WebhookRequest request = new WebhookRequest();
+        List<String> events = new ArrayList<String>();
+        events.add("ALL");
+        request.setEvents(events);
+        request.setWebHookUrl("invalid-url");
+        try {
+            WebhookResponse response = webhookService.create(PropertyUtils.getProperty("merchantId"), request);
+        } catch (WorldpayException e) {
+            assertThat("Invalid event type", e.getApiError().getCustomCode(), is("BAD_REQUEST"));
+            assertThat("Invalid event type", e.getApiError().getMessage(), is("URL is not valid"));
+        }
+    }
+
+    @Test
+    public void shouldGetWebhooksForValidMerchant() {
+        WebhookListResponse response = webhookService.getWebhooks(PropertyUtils.getProperty("merchantId"));
+        assertThat("webhooks for the given merchant", response.getWebHooks().size(), is(greaterThan(0)));
+    }
+
+    @Test
+    public void shouldNotGetWebhooksForInvalidMerchant() {
+        try {
+            webhookService.getWebhooks("invalid-merchant");
+        } catch (WorldpayException e) {
+            assertThat("Access should be denied", e.getApiError().getCustomCode(), is("ACCESS_DENIED"));
+            assertThat("Access should be denied", e.getApiError().getMessage(), is("Access is denied"));
+        }
+
+    }
+
+
     /**
      * Utility to build the string representation of the data to expect via the webhook notification
      */
     private String getNotificationString() {
         OrderStatusChangeNotificationPayload statusPayload =
-                new OrderStatusChangeNotificationPayload(
-                        merchantId,
-                        aggregateMerchantId,
-                        adminCode,
-                        merchantCode,
-                        orderCode,
-                        paymentStatus,
-                        environment);
+            new OrderStatusChangeNotificationPayload(merchantId, aggregateMerchantId, adminCode, merchantCode,
+                                                     orderCode, paymentStatus, environment);
 
         return JsonParser.toJson(statusPayload);
     }

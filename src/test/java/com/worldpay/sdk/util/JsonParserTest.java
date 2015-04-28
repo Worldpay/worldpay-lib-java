566d7df660a1cd25707cc86caff749e3c89c1872
nitin-tiwari <nitinjdk@gmail.com>
2015-04-28 15:50:23 +0100

    WPCWTWO-1655: Extracted client-core into a separate repo

--- src/test/java/com/worldpay/sdk/util/JsonParserTest.java
+++ src/test/java/com/worldpay/sdk/util/JsonParserTest.java
@@ -1,6 +1,5 @@
 package com.worldpay.sdk.util;
 
-import com.worldpay.gateway.clearwater.client.core.dto.response.OrderResponse;
 import com.worldpay.gateway.clearwater.client.core.dto.response.TokenResponse;
 import org.junit.Test;
 
@@ -15,31 +14,6 @@ public class JsonParserTest {
 
     @Test
     public void canParseToJson() {
-        /*String orderResponse =
-            "{\n" + "  \"orderCode\" : \"3ceaac2a-4a8e-4c12-b5d9-855ce2547f20\",\n" + "  \"amount\" : 1200,\n"
-            + "  \"currencyCode\" : \"EUR\",\n" + "  \"paymentStatus\" : \"SUCCESS\",\n" + "  \"paymentResponse\" : {\n"
-            + "    \"type\" : \"ObfuscatedCard\",\n" + "    \"name\" : \"Shopper Name\",\n"
-            + "    \"expiryMonth\" : 2,\n" + "    \"expiryYear\" : 2017,\n" + "    \"issueNumber\" : 1,\n"
-            + "    \"startMonth\" : 1,\n" + "    \"startYear\" : 2011,\n" + "    \"cardType\" : \"MASTERCARD\",\n"
-            + "    \"maskedCardNumber\" : \"**** **** **** 1111\"\n" + "  },\n"
-            + "  \"customerOrderCode\" : \"CustomerOrderCode\"\n" + ", \"newPropertyToIgnore\":\"somevalue\"}";
-        OrderResponse response = JsonParser.toObject(orderResponse, OrderResponse.class);
-        assertThat("Response", response, not(nullValue()));
-
-        orderResponse =
-            "{\n" + "  \"orderCode\" : \"1fc06655-7874-4aac-afaf-5c568e2d8021\",\n" + "  \"amount\" : 1200,\n"
-            + "  \"currencyCode\" : \"EUR\",\n" + "  \"paymentStatus\" : \"SUCCESS\",\n" + "  \"paymentResponse\" : {\n"
-            + "    \"type\" : \"ObfuscatedCard\",\n" + "    \"name\" : \"Shopper Name\",\n"
-            + "    \"expiryMonth\" : 2,\n" + "    \"expiryYear\" : 2017,\n" + "    \"issueNumber\" : 1,\n"
-            + "    \"startMonth\" : 1,\n" + "    \"startYear\" : 2011,\n" + "    \"cardType\" : \"MASTERCARD\",\n"
-            + "    \"maskedCardNumber\" : \"**** **** **** 1111\"\n" + "  },\n"
-            + "  \"customerOrderCode\" : \"CustomerOrderCode\",\n" + "  \"disputes\" : [ {\n"
-            + "    \"id\" : \"docId\",\n" + "    \"documentName\" : \"sampleName.txt\",\n"
-            + "    \"creationDate\" : \"12/03/2015\"\n" + "  } ]\n" + "}";
-
-        response = JsonParser.toObject(orderResponse, OrderResponse.class);
-        assertThat("Response without additional properties", response, not(nullValue()));*/
-
         String json =
             " {\n \"token1111\" : \"valid_token\",\n \"paymentMethod\" : {\n \"type\" : \"ObfuscatedCard\",\n \"name\" : \"Shopper Name\",\n \"expiryMonth\" : 2,\n \"expiryYear\" : 2017,\n \"issueNumber\" : 1,\n \"startMonth\" : 1,\n \"startYear\" : 2011,\n \"cardType\" : \"MASTERCARD\",\n \"maskedCardNumber\" : \"**** **** **** 1111\"\n },\n \"reusable\" : true\n}";
         TokenResponse tr = JsonParser.toObject(json, TokenResponse.class);

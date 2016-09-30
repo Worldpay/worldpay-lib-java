f6388ad02da3d75e85b7f45088147afe183432b6
fichp <peter.fich@worldpay.com>
2016-09-30 16:52:49 +0100

    CPB-5744 - Add call to do server-side tokenisation.

--- src/test/java/com/worldpay/sdk/integration/AssertTokenResponseStage.java
+++ src/test/java/com/worldpay/sdk/integration/AssertTokenResponseStage.java
@@ -2,6 +2,7 @@ package com.worldpay.sdk.integration;
 
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.worldpay.gateway.clearwater.client.core.dto.response.CardResponse;
 import com.worldpay.gateway.clearwater.client.core.dto.response.TokenResponse;
 import com.worldpay.gateway.clearwater.client.core.exception.WorldpayException;
 import com.worldpay.sdk.util.PropertyUtils;
@@ -9,6 +10,8 @@ import com.worldpay.sdk.util.PropertyUtils;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.startsWith;
 
 public class AssertTokenResponseStage extends Stage<AssertTokenResponseStage> {
 
@@ -20,7 +23,6 @@ public class AssertTokenResponseStage extends Stage<AssertTokenResponseStage> {
 
     public AssertTokenResponseStage theTokenIsNotNull() {
         assertThat("Token Response", tokenResponse.getToken(), is(notNullValue()));
-
         return self();
     }
 
@@ -41,6 +43,106 @@ public class AssertTokenResponseStage extends Stage<AssertTokenResponseStage> {
 
     public AssertTokenResponseStage theApiErrorCustomCodeIs(String customCode) {
         assertThat("Invalid token", worldpayException.getApiError().getCustomCode(), is(customCode));
+        return self();
+    }
+
+    public AssertTokenResponseStage theTokenStartsWith(String tokenPrefix) {
+        assertThat(tokenResponse.getToken(), startsWith(tokenPrefix));
+        return self();
+    }
+
+    public AssertTokenResponseStage theCardTypeIs(String cardType) {
+        assertThat(((CardResponse) tokenResponse.getCommonToken().getPaymentMethod()).getCardType(), is(cardType));
+        return self();
+    }
+
+    public AssertTokenResponseStage theMaskedCardNumberIs(String cardNumber) {
+        assertThat(((CardResponse) tokenResponse.getCommonToken().getPaymentMethod()).getMaskedCardNumber(), is(cardNumber));
+        return self();
+    }
+
+    public AssertTokenResponseStage theCardSchemaTypeIs(String cardSchemaType) {
+        assertThat(((CardResponse) tokenResponse.getCommonToken().getPaymentMethod()).getCardDetail().getCardSchemeType(), is(cardSchemaType));
+        return self();
+    }
+
+    public AssertTokenResponseStage theCardSchemaNameIs(String cardSchemaName) {
+        assertThat(((CardResponse) tokenResponse.getCommonToken().getPaymentMethod()).getCardDetail().getCardSchemeName(), is(cardSchemaName));
+        return self();
+    }
+
+    public AssertTokenResponseStage theCardIssuerIs(String cardIssuer) {
+        assertThat(((CardResponse) tokenResponse.getCommonToken().getPaymentMethod()).getCardDetail().getCardIssuer(), is(cardIssuer));
+        return self();
+    }
+
+    public AssertTokenResponseStage theCountryCodeIs(String countryCode) {
+        assertThat(((CardResponse) tokenResponse.getCommonToken().getPaymentMethod()).getCardDetail().getCountryCode(), is(countryCode));
+        return self();
+    }
+
+    public AssertTokenResponseStage theCardClassIs(String cardClass) {
+        assertThat(((CardResponse) tokenResponse.getCommonToken().getPaymentMethod()).getCardDetail().getCardClass(), is(cardClass));
+        return self();
+    }
+
+    public AssertTokenResponseStage theCardProductTypeDescNonContactless(String cardProductType) {
+        assertThat(((CardResponse) tokenResponse.getCommonToken().getPaymentMethod()).getCardDetail().getCardProductTypeDescNonContactless(), is(cardProductType));
+        return self();
+    }
+
+    public AssertTokenResponseStage theCardProductTypeDescContactless(String cardProductType) {
+        assertThat(((CardResponse) tokenResponse.getCommonToken().getPaymentMethod()).getCardDetail().getCardProductTypeDescContactless(), is(cardProductType));
+        return self();
+    }
+
+    public AssertTokenResponseStage thePrepaidIs(String prepaid) {
+        assertThat(((CardResponse) tokenResponse.getCommonToken().getPaymentMethod()).getCardDetail().getPrepaid(), is(prepaid));
+        return self();
+    }
+
+    public AssertTokenResponseStage theExpiryMonthIs(int expityMonth) {
+        assertThat(((CardResponse) tokenResponse.getCommonToken().getPaymentMethod()).getExpiryMonth(), is(expityMonth));
+        return self();
+    }
+
+    public AssertTokenResponseStage theExpiryYearIs(int expityYear) {
+        assertThat(((CardResponse) tokenResponse.getCommonToken().getPaymentMethod()).getExpiryYear(), is(expityYear));
+        return self();
+    }
+
+    public AssertTokenResponseStage theIssuerNumberIsNull() {
+        assertThat(((CardResponse) tokenResponse.getCommonToken().getPaymentMethod()).getIssueNumber(), is(nullValue()));
+        return self();
+    }
+
+    public AssertTokenResponseStage theStartMonthIsNull() {
+        assertThat(((CardResponse) tokenResponse.getCommonToken().getPaymentMethod()).getStartMonth(), is(nullValue()));
+        return self();
+    }
+
+    public AssertTokenResponseStage theStartYearIsNull() {
+        assertThat(((CardResponse) tokenResponse.getCommonToken().getPaymentMethod()).getStartYear(), is(nullValue()));
+        return self();
+    }
+
+    public AssertTokenResponseStage theNameIs(String name) {
+        assertThat(tokenResponse.getCommonToken().getPaymentMethod().getName(), is(name));
+        return self();
+    }
+
+    public AssertTokenResponseStage theBillingAddressIsNull() {
+        assertThat(tokenResponse.getCommonToken().getPaymentMethod().getBillingAddress(), is(nullValue()));
+        return self();
+    }
+
+    public AssertTokenResponseStage theTokenIsNotReusable() {
+        assertThat(tokenResponse.getCommonToken().isReusable(), is(false));
+        return self();
+    }
+
+    public AssertTokenResponseStage theShopperLanguageCodeIsNull() {
+        assertThat(tokenResponse.getCommonToken().getShopperLanguageCode(), is(nullValue()));
         return self();
     }
 }
--- src/test/java/com/worldpay/sdk/integration/TokenServiceCT.java
+++ src/test/java/com/worldpay/sdk/integration/TokenServiceCT.java
@@ -1,6 +1,9 @@
 package com.worldpay.sdk.integration;
 
 import com.tngtech.jgiven.junit.ScenarioTest;
+import com.worldpay.gateway.clearwater.client.core.dto.common.CommonToken;
+import com.worldpay.gateway.clearwater.client.core.dto.request.CardRequest;
+import com.worldpay.gateway.clearwater.client.core.dto.request.TokenRequest;
 import com.worldpay.sdk.util.PropertyUtils;
 import org.junit.Test;
 
@@ -40,11 +43,210 @@ public class TokenServiceCT extends ScenarioTest<EmptyStage, TokenStage, AssertT
      * The Token API throws WorldpayException with the custom code TKN_NOT_FOUND
      */
     @Test
-    public void shouldNotGetInValidToken() {
+    public void shouldNotGetInvalidToken() {
         when()
             .weGetAToken("invalid-token");
 
         then()
             .theApiErrorCustomCodeIs("TKN_NOT_FOUND");
+    }
+
+    @Test
+    public void shouldCreateASingleUseTokenForAVisaCard() {
+        CardRequest cardRequest = new CardRequest();
+        cardRequest.setName("John Doe");
+        cardRequest.setCardNumber("4444333322221111");
+        cardRequest.setExpiryMonth(2);
+        cardRequest.setExpiryYear(2017);
+        cardRequest.setCvc("123");
+
+        CommonToken commonToken = new CommonToken(cardRequest, false);
+
+        TokenRequest tokenRequest = new TokenRequest(commonToken);
+        tokenRequest.setClientKey(PropertyUtils.getProperty("clientKey"));
+
+        when()
+            .weCreateAToken(tokenRequest);
+
+        then()
+            .theTokenStartsWith("TEST_SU_")
+            .and()
+            .theCardTypeIs("VISA_CREDIT")
+            .and()
+            .theMaskedCardNumberIs("**** **** **** 1111")
+            .and()
+            .theCardSchemaTypeIs("consumer")
+            .and()
+            .theCardSchemaNameIs("VISA CREDIT")
+            .and()
+            .theCardIssuerIs("NATWEST")
+            .and()
+            .theCountryCodeIs("GB")
+            .and()
+            .theCardClassIs("credit")
+            .and()
+            .theCardProductTypeDescNonContactless("Visa Credit Personal")
+            .and()
+            .theCardProductTypeDescContactless("CL Visa Credit Pers")
+            .and()
+            .thePrepaidIs("false")
+            .and()
+            .theExpiryMonthIs(2)
+            .and()
+            .theExpiryYearIs(2017)
+            .and()
+            .theIssuerNumberIsNull()
+            .and()
+            .theStartMonthIsNull()
+            .and()
+            .theStartYearIsNull()
+            .and()
+            .theNameIs("John Doe")
+            .and()
+            .theTokenIsNotReusable()
+            .and()
+            .theShopperLanguageCodeIsNull()
+            .and()
+            .theBillingAddressIsNull();
+    }
+
+    @Test
+    public void shouldCreateReusableTokenForAnAmexCard() {
+        CardRequest cardRequest = new CardRequest();
+        cardRequest.setName("John Doe");
+        cardRequest.setCardNumber("34343434343434");
+        cardRequest.setExpiryMonth(2);
+        cardRequest.setExpiryYear(2017);
+        cardRequest.setCvc("123");
+
+        CommonToken commonToken = new CommonToken(cardRequest, false);
+
+        TokenRequest tokenRequest = new TokenRequest(commonToken);
+        tokenRequest.setClientKey(PropertyUtils.getProperty("clientKey"));
+
+        when()
+            .weCreateAToken(tokenRequest);
+
+        then()
+            .theTokenStartsWith("TEST_SU_")
+            .and()
+            .theCardTypeIs("AMEX")
+            .and()
+            .theMaskedCardNumberIs("**** **** ** 3434")
+            .and()
+            .theCardSchemaTypeIs("unknown")
+            .and()
+            .theCardSchemaNameIs("unknown")
+            .and()
+            .theCardIssuerIs("unknown")
+            .and()
+            .theCountryCodeIs("XX")
+            .and()
+            .theCardClassIs("unknown")
+            .and()
+            .theCardProductTypeDescNonContactless("unknown")
+            .and()
+            .theCardProductTypeDescContactless("unknown")
+            .and()
+            .thePrepaidIs("unknown")
+            .and()
+            .theExpiryMonthIs(2)
+            .and()
+            .theExpiryYearIs(2017)
+            .and()
+            .theIssuerNumberIsNull()
+            .and()
+            .theStartMonthIsNull()
+            .and()
+            .theStartYearIsNull()
+            .and()
+            .theNameIs("John Doe")
+            .and()
+            .theTokenIsNotReusable()
+            .and()
+            .theShopperLanguageCodeIsNull()
+            .and()
+            .theBillingAddressIsNull();
+    }
+
+    @Test
+    public void shouldCreateReusableTokenForAMaestroCard() {
+        CardRequest cardRequest = new CardRequest();
+        cardRequest.setName("John Doe");
+        cardRequest.setCardNumber("6759649826438453");
+        cardRequest.setExpiryMonth(2);
+        cardRequest.setExpiryYear(2017);
+        cardRequest.setCvc("123");
+
+        CommonToken commonToken = new CommonToken(cardRequest, false);
+
+        TokenRequest tokenRequest = new TokenRequest(commonToken);
+        tokenRequest.setClientKey(PropertyUtils.getProperty("clientKey"));
+
+        when()
+            .weCreateAToken(tokenRequest);
+
+        then()
+            .theTokenStartsWith("TEST_SU_")
+            .and()
+            .theCardTypeIs("MAESTRO")
+            .and()
+            .theMaskedCardNumberIs("**** **** **** 8453")
+            .and()
+            .theCardSchemaTypeIs("unknown")
+            .and()
+            .theCardSchemaNameIs("unknown")
+            .and()
+            .theCardIssuerIs("unknown")
+            .and()
+            .theCountryCodeIs("XX")
+            .and()
+            .theCardClassIs("unknown")
+            .and()
+            .theCardProductTypeDescNonContactless("unknown")
+            .and()
+            .theCardProductTypeDescContactless("unknown")
+            .and()
+            .thePrepaidIs("unknown")
+            .and()
+            .theExpiryMonthIs(2)
+            .and()
+            .theExpiryYearIs(2017)
+            .and()
+            .theIssuerNumberIsNull()
+            .and()
+            .theStartMonthIsNull()
+            .and()
+            .theStartYearIsNull()
+            .and()
+            .theNameIs("John Doe")
+            .and()
+            .theTokenIsNotReusable()
+            .and()
+            .theShopperLanguageCodeIsNull()
+            .and()
+            .theBillingAddressIsNull();
+    }
+
+    @Test
+    public void shouldThrowExceptionIsTokenCreationRequestIsInvalid() {
+        CardRequest cardRequest = new CardRequest();
+        cardRequest.setName("John Doe");
+        cardRequest.setCardNumber("6759649826438453");
+        cardRequest.setExpiryMonth(2);
+        cardRequest.setExpiryYear(2017);
+        cardRequest.setCvc("bad-cvc");
+
+        CommonToken commonToken = new CommonToken(cardRequest, false);
+
+        TokenRequest tokenRequest = new TokenRequest(commonToken);
+        tokenRequest.setClientKey(PropertyUtils.getProperty("clientKey"));
+
+        when()
+            .weCreateAToken(tokenRequest);
+
+        then()
+            .theApiErrorCustomCodeIs("BAD_REQUEST")
+            .theErrorMessageIs("API error: CVC Must always contain valid digits");
     }
 }
